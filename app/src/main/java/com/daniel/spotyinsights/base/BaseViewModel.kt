c38b25f66ec20ee59633626992568bfe96b91bb0
Danylo Bulavinov <99501170+DanyaBulavinov@users.noreply.github.com>
2025-04-02 07:50:09 +0300

    Add BaseViewModel with common functionality

---
+++ app/src/main/java/com/daniel/spotyinsights/base/BaseViewModel.kt
@@ -0,0 +1,54 @@
+package com.daniel.spotyinsights.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel<State : UiState, Event : UiEvent, Effect : UiEffect> : ViewModel() {
+
+    private val initialState: State by lazy { createInitialState() }
+    abstract fun createInitialState(): State
+
+    private val _uiState: MutableStateFlow<State> = MutableStateFlow(initialState)
+    val uiState: StateFlow<State> = _uiState.asStateFlow()
+
+    private val _event: MutableSharedFlow<Event> = MutableSharedFlow()
+    val event: SharedFlow<Event> = _event.asSharedFlow()
+
+    private val _effect: MutableSharedFlow<Effect> = MutableSharedFlow()
+    val effect: SharedFlow<Effect> = _effect.asSharedFlow()
+
+    init {
+        subscribeToEvents()
+    }
+
+    private fun subscribeToEvents() {
+        viewModelScope.launch {
+            event.collect {
+                handleEvent(it)
+            }
+        }
+    }
+
+    abstract fun handleEvent(event: Event)
+
+    protected fun setState(reduce: State.() -> State) {
+        val newState = _uiState.value.reduce()
+        _uiState.value = newState
+    }
+
+    protected fun setEffect(builder: () -> Effect) {
+        val effectValue = builder()
+        viewModelScope.launch { _effect.emit(effectValue) }
+    }
+
+    fun setEvent(event: Event) {
+        viewModelScope.launch { _event.emit(event) }
+    }
+}
